d2443ab65186c7404d22fdf50f1ce62cb7b2672d
Gleb <glebimptab@gmail.com>
2017-09-28 21:48:23 +0300

    TaskBr

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let num = 0;
+  for (let j = str.length; j >= 0; j--) {
+    for (let k = 0; k < bracketsConfig.length; k++) {
+      if (str[j] == bracketsConfig[k][0]) {
+        str = str.replace(bracketsConfig[k].join(''), '');
+      }
+    }
+  }
+  if (str == '') {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
